64670a3db8165c01b0599a5d94618df39c2fd9e2
jschli <jschlicht@student.ethz.ch>
2025-04-05 16:43:46 -0500

    somewhat working

--- HW_2/Button_LED/Button_LED.c
+++ HW_2/Button_LED/Button_LED.c
@@ -9,12 +9,13 @@ volatile uint32_t press_count = 0;
 volatile bool led_state = false;
 
 void gpio_callback(uint gpio, uint32_t events) {
-    if (gpio == BUTTON_PIN && (events & GPIO_IRQ_EDGE_RISE)) {
+    //if (gpio == BUTTON_PIN && (events & GPIO_IRQ_EDGE_RISE)) {
         press_count++;
         led_state = !led_state;
         gpio_put(LED_PIN, led_state);
         printf("Button pressed %d times\n", press_count);
-    }
+        sleep_ms(1000);
+    //}
 }
 
 int main()
@@ -29,18 +30,12 @@ int main()
 
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN); 
+    gpio_pull_down(BUTTON_PIN); 
 
-    // Register and enable the interrupt
-    gpio_set_irq_enabled_with_callback(
-        BUTTON_PIN,
-        GPIO_IRQ_EDGE_RISE,
-        true,
-        &gpio_callback
-    );
+
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
     while (true) {
-        // Main loop can stay quiet or print status if you like
-        sleep_ms(1000);
+
     }
 }
